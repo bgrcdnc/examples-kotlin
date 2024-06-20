1109060d17119946a85cd576ed9b4a744bcb6331
bgrcdnc <bugracaliskandinc@gmail.com>
2024-06-21 00:47:43 +0300

    reformat code

--- playground/src/main/kotlin/Main.kt
+++ playground/src/main/kotlin/Main.kt
@@ -4,21 +4,18 @@ fun main() {
     nameNSurname()
 }
 
-fun nameNSurname () {
+fun nameNSurname() {
 //    print("What is your name? : ")
     val name: String = "Buğra" // readln()
-
 //    print("What is your surname, $name? : ")
     val surname: String = "Çalışkandinç" // readln()
-
 //    print("What is your age, $name? : ")
     val age: Int = 27 // readln().toInt()
-
 //    print("What is your gender, $name? (True: Male, False: Female) : ")
     val gender: Boolean = true // readln().toBoolean()
 
     print("Hello, $name $surname; aged $age, gender ")
-    if(gender) print("Male")
+    if (gender) print("Male")
     else print("Female")
 }
 
