c93335e3c324ea3d95b2ffae7dc276a0c5eb3363
bgrcdnc <bugracaliskandinc@gmail.com>
2024-06-21 00:32:21 +0300

    feat gender, initialized values for debug

--- playground/src/main/kotlin/Main.kt
+++ playground/src/main/kotlin/Main.kt
@@ -1,11 +1,24 @@
 package org.example
 
 fun main() {
-    val name: String = "Buğra" //explicitly given type due to practice - ignore warning
-    val age = 27
-
-    print("What is your surname, $name? : ")
-    val surname: String = readln()
-
-    print("Hello, $name $surname; aged $age")
+    nameNSurname()
 }
+
+fun nameNSurname () {
+//    print("What is your name? : ")
+    val name: String = "Buğra" // readln()
+
+//    print("What is your surname, $name? : ")
+    val surname: String = "Çalışkandinç" // readln()
+
+//    print("What is your age, $name? : ")
+    val age: Int = 27 // readln().toInt()
+
+//    print("What is your gender, $name? (True: Male, False: Female) : ")
+    val gender: Boolean = true // readln().toBoolean()
+
+    print("Hello, $name $surname; aged $age, gender ")
+    if(gender) print("Male")
+    else print("Female")
+}
+
