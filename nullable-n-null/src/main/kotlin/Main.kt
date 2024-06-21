671d5b3459344e6b7f77db273d425d3eb9ca4bec
bgrcdnc <bugracaliskandinc@gmail.com>
2024-06-21 16:48:26 +0300

    Added nullable and null example

--- nullable-n-null/src/main/kotlin/Main.kt
+++ nullable-n-null/src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 package org.example
 
 fun main() {
+    var name: String? = null
+    var age: Int? = null
+    var num = null
+//    num = 5 // Cannot assign to Nothing?
 
+    age = 5
+    name = "Bugra"
+    print("${name?.toLowerCase()}, ${5 + age!!}, $num")
 }
