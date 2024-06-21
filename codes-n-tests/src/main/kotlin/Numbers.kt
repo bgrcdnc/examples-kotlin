a17f9302be4242629fd3616f0f92a3151b75b389
bgrcdnc <bugracaliskandinc@gmail.com>
2024-06-21 22:17:48 +0300

    Added some number operations

--- codes-n-tests/src/main/kotlin/Numbers.kt
+++ codes-n-tests/src/main/kotlin/Numbers.kt
@@ -1,15 +1,32 @@
 package org.example
 
 fun main() {
-    val number: Int = 100 // int
+    println(" ---- Boxed && Unboxed ---- ")
+    val number: Int = 127 // int
     val boxedNumber: Int? = number
     val anotherBoxedNumber: Int? = number
-    println("boxedNumber === anotherBoxedNumber : " +
-            (boxedNumber === anotherBoxedNumber)) // true
+    println(
+        "$number -> boxedNumber === anotherBoxedNumber : " + (boxedNumber === anotherBoxedNumber)
+    ) // true
 
-    val number2: Int = 10000
-    val boxedNumber2 : Int? = number2
-    val anotherBoxedNumber2 : Int? = number2
-    println("boxedNumber2 === anotherBoxedNumber2 : " +
-            (boxedNumber2 === anotherBoxedNumber2)) // false
+    val number2: Int = 128
+    val boxedNumber2: Int? = number2
+    val anotherBoxedNumber2: Int? = number2
+    println(
+        "$number2 -> boxedNumber2 === anotherBoxedNumber2 : " + (boxedNumber2 === anotherBoxedNumber2)
+    ) // false
+
+    /** Type conversion **/
+//    implicit type conversion yok -> var number: Int = (Int) 13L
+    println("\n ---- Type Conversion ---- ")
+
+//    Type conversion yapmak için açık bir şekilde dönüştürmek lazım
+    val intValue: Int = (13L).toInt()
+    println("(${intValue::class.qualifiedName} - ${intValue::class.simpleName}) - intValue: $intValue (from 13L with toInt())")
+
+//    Number Auto-cast
+    println("\n ---- Number Type Cast ---- ")
+    val val1 = arrayListOf(34, 34L, 34.0, 34.0F, "3401283091829827394827", (34).toShort(), (34).toByte())
+    for (num in val1) println("value '$num' is '${num::class.simpleName}'")
+
 }
