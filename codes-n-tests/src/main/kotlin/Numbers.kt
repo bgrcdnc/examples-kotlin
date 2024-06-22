885fcad6d03ff05fa6b50dca73ec6edc8e0ecc8f
bgrcdnc <bugracaliskandinc@gmail.com>
2024-06-22 17:10:00 +0300

    Finalize Numbers.kt

--- codes-n-tests/src/main/kotlin/Numbers.kt
+++ codes-n-tests/src/main/kotlin/Numbers.kt
@@ -1,5 +1,7 @@
 package org.example
 
+fun printType(value: Any) = println("value '$value' is '${value::class.simpleName}'")
+
 fun main() {
     println(" ---- Boxed && Unboxed ---- ")
     val number: Int = 127 // int
@@ -19,14 +21,20 @@ fun main() {
     /** Type conversion **/
 //    implicit type conversion yok -> var number: Int = (Int) 13L
     println("\n ---- Type Conversion ---- ")
-
 //    Type conversion yapmak için açık bir şekilde dönüştürmek lazım
     val intValue: Int = (13L).toInt()
-    println("(${intValue::class.qualifiedName} - ${intValue::class.simpleName}) - intValue: $intValue (from 13L with toInt())")
+    println("(${intValue::class.qualifiedName} - ${intValue::class.simpleName}) - intValue: $intValue (from 13L with toInt())") // Int
 
 //    Number Auto-cast
     println("\n ---- Number Type Cast ---- ")
     val val1 = arrayListOf(34, 34L, 34.0, 34.0F, "3401283091829827394827", (34).toShort(), (34).toByte())
-    for (num in val1) println("value '$num' is '${num::class.simpleName}'")
+    for (num in val1) printType(num)
 
+//    Number Auto-cast in declaration
+    println("\n ---- Number Declare Cast ---- ")
+    val num2:Byte = 100
+    printType(num2) // Byte
+    val additionCast = num2 + 1239823749L
+    printType(additionCast) // Long
+//    Buyuk olan sayinin tipine gore autocast yapar
 }
