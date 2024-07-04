81dfb35534b3adbcd1f1dbbaa24e6dfdffbb8e14
bgrcdnc <bugracaliskandinc@gmail.com>
2024-07-04 17:58:30 +0300

    Changed Colors to class type

--- codes-n-tests/src/main/kotlin/Utils.kt
+++ codes-n-tests/src/main/kotlin/Utils.kt
@@ -1,42 +1,46 @@
 package org.example
 
-object Foreground_ {
-    const val Black: String         = "\u001B[30m"
-    const val Red: String           = "\u001B[91m"
-    const val Green: String         = "\u001B[92m"
-    const val Yellow: String        = "\u001B[93m"
-    const val Blue: String          = "\u001B[94m"
-    const val Magenta: String       = "\u001B[95m"
-    const val Cyan: String          = "\u001B[96m"
-    const val Gray: String          = "\u001B[90m"
-    const val White: String         = "\u001B[98m"
+class Colors {
+    companion object {
+        const val Reset: String = "\u001B[0m"
+        const val Bold: String = "\u001B[1m"
+    }
 
-    const val DarkRed: String     = "\u001B[31m"
-    const val DarkGreen: String   = "\u001B[32m"
-    const val DarkYellow: String  = "\u001B[33m"
-    const val DarkBlue: String    = "\u001B[34m"
-    const val DarkMagenta: String = "\u001B[35m"
-    const val DarkCyan: String    = "\u001B[36m"
-    const val DarkGray: String    = "\u001B[37m"
-    const val DarkWhite: String   = "\u001B[38m"
-}
+    class Background {
+        companion object {
+            const val Black: String = "\u001B[40m"
+            const val Red: String = "\u001B[41m"
+            const val Green: String = "\u001B[42m"
+            const val Yellow: String = "\u001B[43m"
+            const val Blue: String = "\u001B[44m"
+            const val Magenta: String = "\u001B[45m"
+            const val Cyan: String = "\u001B[46m"
+            const val White: String = "\u001B[47m"
+        }
+    }
 
-object Background_ {
-    const val Black: String         = "\u001B[40m"
-    const val Red: String           = "\u001B[41m"
-    const val Green: String         = "\u001B[42m"
-    const val Yellow: String        = "\u001B[43m"
-    const val Blue: String          = "\u001B[44m"
-    const val Magenta: String       = "\u001B[45m"
-    const val Cyan: String          = "\u001B[46m"
-    const val White: String         = "\u001B[47m"
-}
+    class Foreground {
+        companion object {
+            const val Black: String = "\u001B[30m"
+            const val Red: String = "\u001B[91m"
+            const val Green: String = "\u001B[92m"
+            const val Yellow: String = "\u001B[93m"
+            const val Blue: String = "\u001B[94m"
+            const val Magenta: String = "\u001B[95m"
+            const val Cyan: String = "\u001B[96m"
+            const val Gray: String = "\u001B[90m"
+            const val White: String = "\u001B[98m"
 
-object Colors {
-    const val Reset: String = "\u001B[0m"
-    const val Bold: String = "\u001B[1m"
-    val Foreground = Foreground_
-    val Background = Background_
+            const val DarkRed: String = "\u001B[31m"
+            const val DarkGreen: String = "\u001B[32m"
+            const val DarkYellow: String = "\u001B[33m"
+            const val DarkBlue: String = "\u001B[34m"
+            const val DarkMagenta: String = "\u001B[35m"
+            const val DarkCyan: String = "\u001B[36m"
+            const val DarkGray: String = "\u001B[37m"
+            const val DarkWhite: String = "\u001B[38m"
+        }
+    }
 }
 
 fun printVariable(name: String, value: Any) {
